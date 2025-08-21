345974a9ea6b8f2ee66159c68c3a3a0c32040e8c
MTRNord <MTRNord@users.noreply.github.com>
2025-08-21 19:12:50 +0200

    Fix config

--- yate/docker/startup.sh
+++ yate/docker/startup.sh
@@ -1,15 +1,15 @@
-#!/bin/sh
+#!/usr/bin/env bashio
 rm -r /yate/conf.d
 ln -s /config /yate/conf.d
 
-# Replace HASSIO_MYSQL_*
+# Replace MySQL configuration values
 # if /config/mysqldb.conf exists.
 if [ -f /config/mysqldb.conf ]; then
-    sed -i "s/HASSIO_MYSQL_HOST/$(bashio::services mysql "host")/g" /config/mysqldb.conf
-    sed -i "s/HASSIO_MYSQL_PORT/$(bashio::services mysql "port")/g" /config/mysqldb.conf
-    sed -i "s/HASSIO_MYSQL_USER/$(bashio::config "mysql_user")/g" /config/mysqldb.conf
-    sed -i "s/HASSIO_MYSQL_PASSWORD/$(bashio::config "mysql_password")/g" /config/mysqldb.conf
-    sed -i "s/HASSIO_MYSQL_DB/$(bashio::config "mysql_db")/g" /config/mysqldb.conf
+    sed -i "s/^host=.*/host=$(bashio::services mysql "host")/g" /config/mysqldb.conf
+    sed -i "s/^port=.*/port=$(bashio::services mysql "port")/g" /config/mysqldb.conf
+    sed -i "s/^user=.*/user=$(bashio::config "mysql_user")/g" /config/mysqldb.conf
+    sed -i "s/^password=.*/password=$(bashio::config "mysql_password")/g" /config/mysqldb.conf
+    sed -i "s/^database=.*/database=$(bashio::config "mysql_db")/g" /config/mysqldb.conf
 fi
 
 /usr/bin/supervisord
